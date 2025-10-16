4ded59c683bba626be082e1eb2976f1319aace1b
kavipriyamunusamy-04 <kavipriya.munusamy@valeo.com>
2025-10-16 11:01:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Dashboard/',    // 👈 Must match repo name exactly (case-sensitive)
   plugins: [react()],
 })
-
-
-
-
